b8e6ad12fff63c4c3bd08986cd05181a19bca295
shilpa <36531617+shilpathota@users.noreply.github.com>
2021-04-04 01:30:44 +0530

    Create Main.java

---
+++ src/com/techiebees/Main.java
@@ -0,0 +1,29 @@
+package com.techiebees;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+            lambdaexpression.executeExample();
+
+            MethodReferences.executeMethodReferences();
+
+            FunctionalInterfaces.executeFunctionalInterfaces();
+
+            StreamAPI.executeStream();
+
+            streamfilter.executestreamfilter();
+
+            base64encodedecode.executeencodedecode();
+
+            DefaultMethods.executedefaultmethods();
+
+            forEach.executeForeach();
+
+            collectors.executecollectors();
+
+            stringjoiner.executestringjoiner();
+
+            optional.executeoptional();
+    }
+}
